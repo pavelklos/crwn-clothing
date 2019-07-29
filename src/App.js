07f4fc645866b574a540ce2b4ba9e60131125210
pavelklos <pavelkloscz@gmail.com>
2019-07-29 10:27:18 +0200

    updating our app with a shop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 const HatsPage = () => (
   <div>
@@ -33,6 +34,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
         <Route exact path='/test' component={TestPage} />
         <Route path='/test/:id' component={TestPage} />
       </Switch>
---
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import CollectionItem from '../collection-item/collection-item.component';
+
+import './collection-preview.styles.scss';
+
+const CollectionPreview = ({ title, routeName, items }) => (
+  <div className='collection-preview'>
+    <h1 className='title'>{title.toUpperCase()}</h1>
+    <b style={{ color: 'red' }} className='title'>{routeName}</b>
+    <div className='preview'>
+      {
+        // items.map((item) => (
+        //   <div key={item.id}>{item.id} - <b>{item.name}</b></div>
+        // ))
+        items
+          .filter((item, idx) => idx < 4)
+          // .map((item, idx) => (
+            // <div key={item.id}>[{idx}] {item.id} - <b>{item.name}</b> - ${item.price}</div>
+          .map(({id, ...otherItemProps}) => (
+            <CollectionItem key={id} {...otherItemProps}/>
+        ))
+      }
+    </div>
+  </div>
+)
+
+export default CollectionPreview;
